e1fee0912056a92ab766c58d605eee230e684f2e
Sanjit Dandapanthula <sanjitd360@gmail.com>
2023-05-28 13:30:06 -0700

    wrote a wrapper class for shell state

--- src/util/shells.py
+++ src/util/shells.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-
-def roll_shells():
-    # use empirical probabilities computed in section 4.1 of this paper:
-    # https://iaeme.com/MasterAdmin/Journal_uploads/IJCET/VOLUME_10_ISSUE_1/IJCET_10_01_019.pdf
-    rolls = [1, 2, 3, 4, 8]
-    empirical_probabilities = [0.243, 0.381, 0.236, 0.074, 0.066]
-    return np.random.choice(rolls, size=(1,), p=empirical_probabilities)
+class Shells:
+    def __init__(self):
+        self.__state = 4
+    
+    def roll_shells(self):
+        # use empirical probabilities computed in section 4.1 of this paper:
+        # https://iaeme.com/MasterAdmin/Journal_uploads/IJCET/VOLUME_10_ISSUE_1/IJCET_10_01_019.pdf
+        empirical_probabilities = [0.243, 0.381, 0.236, 0.074, 0.066]
+        self.__state = np.random.choice(np.arange(5), size=(1,), p=empirical_probabilities)
+    
+    @property
+    def state(self):
+        return self.__state
